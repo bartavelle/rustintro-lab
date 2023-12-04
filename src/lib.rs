391bb7ba40b7d475304718c3228b17e71e080fbe
Bartavelle <bartavelle@gmail.com>
2023-12-04 10:43:39 +0100

    chore: prepare for lab

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 // uncomment each module, one by one, and fix the tests
 
-// mod intro;
+mod intro;
 // mod ownership;
 // mod error;
 // mod traits;
-pub mod iterators_create;
-pub mod iterators_use;
-pub mod tennis;
+// pub mod iterators_use;
+// pub mod iterators_create;
+// pub mod tennis;
